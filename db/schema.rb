9f0a26d01f17ef7788430f9f814980909f125fb7
Djo-75 <j.pecquet@live.fr>
2023-06-06 10:12:52 +0200

    devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,5 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_06_075647) do
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
-  end
-
+ActiveRecord::Schema[7.0].define(version: 0) do
 end
