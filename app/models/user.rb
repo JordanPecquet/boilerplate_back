87418af52606bb31df053f414fef4f6b39e0af53
DjoLaDouille <djord28_gaulois@live.fr>
2023-06-08 18:28:34 +0200

    authentification, cors etc

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtDenylist
+  has_many :articles
 
-         has_many :articles
-
-        #  validates :email, presence: true
+  #  validates :email, presence: true
 end
